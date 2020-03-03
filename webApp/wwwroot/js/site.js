ae7c6f8f70cf016f5705174efe8ae74527f0ddce
okarstrom <k4rstrom@hotmail.com>
2020-03-03 15:07:27 +0100

    login/register html/css complete

--- webApp/wwwroot/js/site.js
+++ webApp/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+
+function termsPopup() {
+  var x = document.getElementById("snackbar");
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
+function openSignIn() {
+    document.getElementById('id02').style.display='none';
+    document.getElementById('id01').style.display='block';
+}
+
+function openRegister() {
+    document.getElementById('id01').style.display='none';
+    document.getElementById('id02').style.display='block';
+}
